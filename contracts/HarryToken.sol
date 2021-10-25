20c882348a0a6ef297c1997b390b7f5389d30bbd
raptoreth <harryswap@protonmail.com>
2021-10-25 18:53:41 +0200

    Adding token and masterchef contract

---
+++ contracts/HarryToken.sol
@@ -0,0 +1,13 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity 0.6.12;
+
+import "./libs/BEP20.sol";
+
+contract HarryToken is BEP20('HarrySwap', 'HARRY') {
+
+    /// @notice Creates `_amount` token to `_to`. Must only be called by the owner (MasterChef).
+    function mint(address _to, uint256 _amount) public onlyOwner {
+        _mint(_to, _amount);
+    }
+}
